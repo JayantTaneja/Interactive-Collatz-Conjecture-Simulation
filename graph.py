e786f560695168105faf2f14e897f79c0f2e60d7
Jayant Taneja <jayanttaneja1@gmail.com>
2021-10-07 16:23:05 +0530

    Initital commit

---
+++ graph.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import time, pandas as pd
+
+def c(n):    
+    ans=[]
+    ans.append(n)
+
+    while True:
+        if n<=1:
+            break
+
+        if n%2==0:
+            n /=2
+        else:
+            n = 3 * n+1
+        ans.append(n)
+    
+    return ans
+
+def plot(chart, speed, y):
+    if speed=="Fast":
+        speed=0.001
+    else:
+        speed=2
+    iterat=st.text("No of iterations : 0")
+    progress_bar = st.progress(0)
+    status_text = st.empty()
+
+    y1=[]
+
+    j=1
+
+    for i in y:
+        y1.append(i)
+        chart.line_chart(y1)
+        iterat.write("No of iterations : {}".format(j))
+        progress=int(j/len(y) *100)
+        status_text.text("%i%% Complete" % progress)
+        progress_bar.progress(progress)
+        j += 1
+        time.sleep(speed)
+
+    
+    d={"Iteration":[i+1 for i in range(len(y))], "Value":y}
+    with st.expander("Show Details"):
+        st.dataframe(pd.DataFrame(d,index=[i for i in range(len(y))]))
+    time.sleep(10)
+    progress_bar.empty()
+    status_text.empty()
---
+++ main.py
@@ -0,0 +1,55 @@
+import streamlit as st
+from graph import c, plot
+
+#Page Config 
+st.title("Interactive Collatz Conjecture Simulation")
+
+st.write("Choose the starting point of this Graph from the side bar to the left $\leftarrow$")
+
+#Default Plot
+chart=st.line_chart(c(5))
+
+st.write("Try zooming in or out by scrolling over the graph")
+
+#Sidebar
+n = st.sidebar.slider(label="starting point", min_value=2, max_value=100, value=5, help="Sets where to start the animation")
+speed = st.sidebar.select_slider(label="Animation Speed", value="Fast", options=["Slow", "Fast"], help="Choose to Speed Of Animation")
+begin=st.sidebar.button(label="Plot")
+
+if begin:
+    plot(chart,speed, c(n))
+    
+st.write("\n")
+st.markdown('''
+# What is The Collatz Conjecture?
+\n
+>#### _The simplest Math Problem no one can solve!!_
+>#### _:- Derek Miller, Veritasium_''')
+
+'''
+According to Wikipedia
+\n
+>The Collatz conjecture is a conjecture in mathematics that concerns sequences defined as follows: start with any positive integer n. Then each term is obtained from the previous term as follows: if the previous term is even, the next term is one half of the previous term. If the previous term is odd, the next term is 3 times the previous term plus 1. **The conjecture is that no matter what value of n, the sequence will always reach 1**
+'''
+
+'''
+In other words, \n
+1. Pick any natural number n\n
+2. If it is even, divide by 2 ($\\implies \\frac{n}{2}$)
+\n
+3. If it is odd, multiply by 3 and add 1 ($\implies 3 \\times n +1$)
+\n 4. Repeat 
+\n
+The conjecture is that no matter the value of this number n, the procedure ends up in the loop $4\\rightarrow2\\rightarrow1$
+'''
+
+'''
+#### The weird part
+\n\n
+Despite the algorithm being so simple, we still haven't been able to solve this problem and by solving, I mean we haven't been able to prove this conjecture
+\n\n
+
+And no even the most powerful computers haven't been able to solve the problem. 
+
+[As of 2020, numbers as large as $2^{68}$ as a starting point have been checked](https://en.wikipedia.org/wiki/Collatz_conjecture#Experimental_evidence)
+'''
